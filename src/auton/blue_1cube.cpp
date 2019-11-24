1e22d44d71bbf2b66245c7c2d59a7e1cc5be6231
Sean Yao <seanyao314@gmail.com>
2019-11-23 22:39:08 -0500

    working blue 3 cube auton

--- src/auton/blue_1cube.cpp
+++ src/auton/blue_1cube.cpp
@@ -9,22 +9,8 @@ void blue_1cube() {
     arm_drive(0);
 
     pros::delay(500);
-    intake_drive(200,200);
+    intake_drive(-200,-200);
     chassis.moveDistance(36_in);
-    chassis.moveDistance(-40_in);
-    pros::delay(500);
-    chassis.moveDistance(10_in);
-    chassis.turnAngle(-105);
-    chassis.moveDistance(10_in);
 
-    lever_drive(200);
-    pros::delay(2500);
-    lever_drive(0);
-    chassis.setMaxVelocity(200);
-    chassis.moveDistance(2_in);
-    intake_drive(-200,200);
-    pros::delay(300);
-    intake_drive(0,0);
-    chassis.setMaxVelocity(90);
-    chassis.moveDistance(-5_in);
+
 }
--- src/auton/blue_4cube.cpp
+++ src/auton/blue_4cube.cpp
@@ -10,41 +10,29 @@ void blue_4cube() {
 
     chassis.setMaxVelocity(80);
     intake_drive(100, 100);
-    // chassis.moveDistance(10_in);
-    // chassis.moveDistance(-10_in);
-    // arm_drive(4);
-    // pros::delay(3000);
-    // arm_drive(0);
-    // pros::delay(2000);
-    // chassis.turnAngle(-5);
-    //intake_drive(-200,200);     
-    //chassis.moveDistance(5_in);
-    // pros::delay(10);
-    chassis.moveDistance(26_in);
-    chassis.moveDistance(24_in);
+
+    chassis.moveDistance(50.5_in);
     pros::delay(1000);
     intake_drive(0,0);
-    chassis.setMaxVelocity(200);
-    chassis.moveDistance(-48.5_in);
+    chassis.setMaxVelocity(150);
+    chassis.moveDistance(-45_in);
     pros::delay(500);
-    chassis.setMaxVelocity(30);
-    chassis.moveDistance(-9.5_in);
-    chassis.moveDistance(7.5_in);
-    chassis.turnAngle(114.5_deg);
-    intake_drive(-50,-50);
-    pros::delay(300);
+    chassis.setMaxVelocity(100);
+    chassis.turnAngle(115.5_deg);
+    intake_drive(-200,-200);
+    pros::delay(200);
     intake_drive(0,0);
-    chassis.moveDistance(12_in);
+    chassis.moveDistance(15.5_in);
     lever_drive(200);
     pros::delay(2500);
     lever_drive(0);
     chassis.setMaxVelocity(200);
     chassis.moveDistance(2_in);
     intake_drive(-200,200);
-    pros::delay(300);
+    pros::delay(400);
     intake_drive(0,0);
     chassis.setMaxVelocity(90);
-    chassis.moveDistance(-5_in);
+    chassis.moveDistance(-8_in);
 
 
     //chassis.turnAngle(90_deg);
